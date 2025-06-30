ffbecdb3e4861b53d1330faa356bb08c32180005
MLzappy <plpietrekyt@gmail.com>
2025-06-30 14:33:51 +0200

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from openai import OpenAI
 
 client = OpenAI(
-    api_key="sk-proj-...",         # ← Twój pełny klucz API
-    project="proj_xxxxxxxxxxxx"    # ← Twój project_id
+    api_key="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",         
+    project="proj_X97B21pdMBTFHNmESfPYkXR2"
 )
 
 def generate_script():
